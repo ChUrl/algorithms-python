fc04eec19cc8aa275b8d1a127c16306a84b7f34f
ChUrl <christoph.urlacher@protonmail.com>
2022-12-16 13:19:51 +0100

    Init env

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Python Various";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.devshell.url = "github:numtide/devshell";
+
+  outputs = { self, nixpkgs, flake-utils, devshell }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ devshell.overlay ];
+        };
+
+        myPython = pkgs.python310.withPackages (p: with p; [
+          # Basic
+          rich
+
+          numpy
+          matplotlib
+        ]);
+      in {
+        devShell = pkgs.devshell.mkShell {
+          name = "Python Various";
+
+          packages = with pkgs; [
+            myPython
+            nodePackages.pyright # LSP
+          ];
+
+          # Use $1 for positional args
+          commands = [
+            # {
+            #   name = "";
+            #   help = "";
+            #   command = "";
+            # }
+          ];
+        };
+      });
+}
